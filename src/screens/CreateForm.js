01cfa1750aa4dda2ae6045495edfee4704638650
ogulcanpirim <opirim@gmail.com>
2022-02-18 12:09:14 +0300

    create form document with firebase

--- src/screens/CreateForm.js
+++ src/screens/CreateForm.js
@@ -1,29 +1,42 @@
-import React,{useState} from 'react';
+import React, { useState } from 'react';
 import styles from '../screens/styles';
-import { SafeAreaView, TouchableOpacity, Text, TextInput, Alert} from 'react-native';
+import { View, SafeAreaView, TouchableOpacity, Text, TextInput, Alert, ActivityIndicator} from 'react-native';
 import AntDesign from 'react-native-vector-icons/AntDesign';
+import { auth, db } from '../../Firebase/firebase';
 
 const CreateFormScreen = (props) => {
 
     const [title, setTitle] = useState('');
     const [message, setMessage] = useState('');
+    const [loading, setLoading] = useState(false);
 
     const goBack = () => {
         props.navigation.goBack();
     }
 
-    const createForm = () => {
-        if (title.length < 5){
+    const createForm = async () => {
+        setLoading(true);
+        if (title.length < 5) {
             Alert.alert("Hata", "Forum başlığı en az 5 karakter olmalıdır!")
         }
-        else if (!message){
+        else if (!message) {
             Alert.alert("Hata", "Forum mesajı boş olamaz!");
         }
         else {
             //TODO: check whether given entry exists with the same form name
             //TODO: create new document in firebase/forums.
-            Alert.alert("Success", "Forum başarıyla oluşturuldu.");
+            await db.collection("forms").doc().set({ createdUser: auth.currentUser?.uid, formTitle: title, messages: [message] })
+            Alert.alert("Success", '"' + title + '"' + " başlıklı forum başarıyla oluşturuldu.");
         }
+        setLoading(false);
+    }
+
+    if (loading) {
+        return (
+            <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+                <ActivityIndicator size="large" color="#26931e"></ActivityIndicator>
+            </View>
+        );
     }
 
     return (
@@ -39,8 +52,8 @@ const CreateFormScreen = (props) => {
             </TextInput>
             <TextInput value={message} onChangeText={setMessage} style={styles.createFormInput} placeholder='Forum için ilk mesajı yazın...' multiline={true}>
             </TextInput>
-            
-            <TouchableOpacity style={styles.createFormButton} onPress={createForm}>
+
+            <TouchableOpacity disabled={loading} style={styles.createFormButton} onPress={createForm}>
                 <Text style={styles.buttonText}>OLUŞTUR</Text>
             </TouchableOpacity>
         </SafeAreaView>
