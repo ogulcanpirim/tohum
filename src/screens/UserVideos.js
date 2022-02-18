be6c98259dc03d345cc7f43ab032e435ef7125ca
ogulcanpirim <opirim@gmail.com>
2022-02-18 10:40:11 +0300

    Started User Video Screen

--- src/screens/UserVideos.js
+++ src/screens/UserVideos.js
@@ -1,23 +1,41 @@
 import React from 'react'
-import { SafeAreaView, Text, TouchableOpacity } from 'react-native';
+import { SafeAreaView, Text, TouchableOpacity, ScrollView } from 'react-native';
 import styles from './styles';
 import AntDesign from 'react-native-vector-icons/AntDesign';
+import SearchBarComponent from '../components/SearchBarComponent';
+import { data } from '../data/video_dummy';
+import VideoComponent from '../components/VideoComponent';
 
 const UserVideoScreen = (props) => {
 
     const goBack = () => {
         props.navigation.goBack();
     }
-    
-    return(
+
+    return (
         <SafeAreaView>
-            <TouchableOpacity style = {styles.returnButton} onPress={goBack}>
+            <TouchableOpacity style={styles.returnButton} onPress={goBack}>
                 <AntDesign
                     name={"back"}
                     size={35}>
                 </AntDesign>
             </TouchableOpacity>
             <Text style={styles.screenHeaderWithLogo}>Videolarım</Text>
+            <SearchBarComponent />
+            <ScrollView style={styles.listViewStyle}>
+                {data.map((video) => {
+                    return (
+                        <VideoComponent
+                            key={video.id}
+                            name={video.videoName}
+                            publisher={video.videoPublisher}
+                            length={video.videoLength}
+                            uri={video.videoUri}
+                            goVideo={() => props.navigation.navigate("VideoComponent", { ...video })}
+                        />
+                    );
+                })}
+            </ScrollView>
         </SafeAreaView>
     );
 
