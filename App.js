93a1f1adc012873043dcf73d3253b42460fcf41c
ogulcanpirim <opirim@gmail.com>
2022-03-31 17:05:18 +0300

    fix gesture warning and pods

--- App.js
+++ App.js
@@ -1,13 +1,9 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
-
+import { LogBox } from 'react-native';
 import React from 'react';
 import NavigatorComponent from './src/components/NavigatorComponent';
+LogBox.ignoreLogs([
+  "[react-native-gesture-handler] Seems like you\'re using an old API with gesture components, check out new Gestures system!",
+]);
 const App = () => {
   return (
     <NavigatorComponent></NavigatorComponent>
